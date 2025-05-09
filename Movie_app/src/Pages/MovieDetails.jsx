1f5bd069f281ea7bcfaed8bc369391c56e6da45b
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2025-05-09 21:22:11 +0300

    set the main functionality to get the date and deal with it then

--- Movie_app/src/Pages/MovieDetails.jsx
+++ Movie_app/src/Pages/MovieDetails.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import { useParams } from "react-router";
-
+import React, { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router";
+import { axiosInstance, axiosImages } from "../apis/config.js";
 export default function MovieDetails() {
+  const [movie, setMovie] = useState();
   const params = useParams();
+
+  const [] = useSearchParams();
+
   console.log(params);
-  return <div>MovieDetails</div>;
+
+  useEffect(() => {
+    axiosInstance
+      .get(`/movie/${params.id}`)
+      .then((res) => setMovie(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+
+  return (
+    <div>
+  <h2>MovieDetails</h2>
+  <hr />
+  </div>
+  );
 }
+
--- Movie_app/src/components/MovieCard.jsx
+++ Movie_app/src/components/MovieCard.jsx
@@ -10,16 +10,16 @@ const reDirectToDetails = (movieID)=>{
 }
 const {movieItem} = props;
   return (
-    <div class="card mb-3" onClick={() => reDirectToDetails(movieItem.id)}>
+    <div className="card mb-3" onClick={() => reDirectToDetails(movieItem.id)}>
       <div className="card-img">
-        <img src={`https://image.tmdb.org/t/p/w500/${movieItem.poster_path}`} class="card-img-top movie-poster" alt={movieItem.title}/>
+        <img src={`https://image.tmdb.org/t/p/w500/${movieItem.poster_path}`} className="card-img-top movie-poster" alt={movieItem.title}/>
       </div>
-    <div class="card-body">
-        <h5 class="card-title">{movieItem.title}</h5>
+    <div className="card-body">
+        <h5 className="card-title">{movieItem.title}</h5>
     </div>
     <div className="card-foot d-flex justify-content-between">  
         <h3 className="mt-2">{movieItem.release_date}</h3>
-        <button><i class="fa-solid fa-heart"></i></button>
+        <button><i className="fa-solid fa-heart"></i></button>
     </div>
   </div>
   );
