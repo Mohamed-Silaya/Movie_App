3fca388ff7f23695589b9d90420cabc3b85d1511
hossamkoky599 <hossam.hassan.0.1.0.1.2@gmail.com>
2025-05-09 19:45:15 +0300

    add paggination in Movie List

--- Movie_app/src/Pages/MovieList.jsx
+++ Movie_app/src/Pages/MovieList.jsx
@@ -3,19 +3,58 @@ import { useEffect, useState } from "react";
 import { axiosInstance } from "../apis/config";
 import "../assets/css/MovieList.css";
 import MovieCard from "../components/MovieCard";
+import { li } from "motion/react-client";
 
 export default function MovieList() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pages, setPages] = useState(1);
+  // useEffect(() => {
+  //   axiosInstance
+  //     .get("/movie/now_playing")
+  //     .then((res) => setMovies(res.data.results))
+  //     .catch((err) => console.log(err))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
   useEffect(() => {
     axiosInstance
-      .get("/movie/popular")
-      .then((res) => setMovies(res.data.results))
+      .get(
+        `/movie/now_playing?page=${currentPage}&api_key=7770c7457a5d7ebb34d378549071963f`
+      )
+      .then((res) => {
+        setMovies(res.data.results);
+        setPages(res.data.total_pages);
+      })
       .catch((err) => console.log(err))
       .finally(() => setIsLoading(false));
-  }, []);
+  }, [currentPage]);
   console.log(movies);
-
+  console.log(pages);
+  console.log(currentPage);
+  const handlePageClick = (newPage)=>{
+    if(newPage >= 1 && newPage <= pages){
+      setCurrentPage(newPage);
+    }
+  }
+  const handlePageNum = ()=>{
+    const pageNum = [];
+    const maxShow =5;
+    const half =Math.floor(maxShow /2);
+    const start = Math.max(currentPage-half,1)
+    const end = Math.min(start+maxShow+1,pages)
+    if (end - start < maxShow -1){
+      start = Math.max(end - maxShow+1,1)
+    }
+    for (let i = start; i <= end; i++) {
+      pageNum.push(
+        <li className={`page-item ${i == currentPage ? "active" : ""}`} key={i}>
+           <button className="page-link" onClick={() => handlePageClick(i)}>{i}</button>
+        </li>
+      )
+    }
+    return pageNum;
+  }
   return (
     <>
       <div className="container">
@@ -29,17 +68,25 @@ export default function MovieList() {
           </div>
         )}
         <div className="row">
-          {movies.map((movie )=> {
+          {movies.map((movie) => {
             return (
               <div className="col-md-3" key={movie.id}>
-                <MovieCard
-                  movieItem={movie}
-                  // onDelete={(id) => handleDelete(id)}
-                />
+                <MovieCard movieItem={movie} />
               </div>
             );
           })}
         </div>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-center mt-3">
+            <li class={`page-item ${currentPage == 1 ? "disabled" : ""}`}>
+                <button className="page-link" onClick={() => handlePageClick(currentPage - 1)}>&laquo;</button>
+            </li>
+            {handlePageNum()}
+            <li class={`page-item ${currentPage == pages ? "disabled" : ""}`}>
+                <button className="page-link" onClick={() => handlePageClick(currentPage - 1)}>&raquo;</button>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
