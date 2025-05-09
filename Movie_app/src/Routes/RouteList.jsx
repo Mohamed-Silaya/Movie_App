6fec2e476b0bd6fcc32d627718679ad9df06f790
Ahmed Abd Elkader 25 <ahmedmohamed251201@gmail.com>
2025-05-09 23:04:57 +0300

    solve RouteList conflict

--- Movie_app/src/Routes/RouteList.jsx
+++ Movie_app/src/Routes/RouteList.jsx
@@ -1,36 +1,21 @@
-<<<<<<< HEAD
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';  // Fixed import
+import { Route, Routes } from 'react-router-dom';
 import MovieTest from '../Pages/MovieList';
 import MovieDetails from '../Pages/MovieDetails';
 import FavoritesPage from '../Pages/FavoritesPage';
+import Footer from '../components/Footer';
 
 function RouteList() {
   return (
-    <Routes>
-      <Route path='/' element={<MovieTest />} />
-      <Route path='/MovieDetails/:id' element={<MovieDetails />} />
-      <Route path='/favorites' element={<FavoritesPage />} />
-    </Routes>
-  );
-=======
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import MovieTest from '../Pages/MovieList'
-import MovieDetails from '../Pages/MovieDetails'
-import Footer from '../components/Footer'
-function RouteList() {
-  return (
-    
     <>
       <Routes>
-          <Route path='/'                  element={<MovieTest/>}/>
-          <Route path='/MovieDetails/:id' element={<MovieDetails/>}/>
+        <Route path='/' element={<MovieTest />} />
+        <Route path='/MovieDetails/:id' element={<MovieDetails />} />
+        <Route path='/favorites' element={<FavoritesPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
-  )
->>>>>>> 14e8dcd233763015412a3ee917b3c79bb4d7c959
+  );
 }
 
 export default RouteList;
