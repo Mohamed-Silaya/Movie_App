8739619fd2cba1efe693114dc1ad6434a1f3ef41
Mohamed-Silaya <mohamedsamer1532@gmail.com>
2025-05-09 22:11:49 +0300

    Adding Footer page and style for it

--- Movie_app/src/Routes/RouteList.jsx
+++ Movie_app/src/Routes/RouteList.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import MovieTest from '../Pages/MovieList'
 import MovieDetails from '../Pages/MovieDetails'
+import Footer from '../components/Footer'
 function RouteList() {
   return (
-    <Routes>
-        <Route path='/'                  element={<MovieTest/>}/>
-        <Route path='/MovieDetails/:id' element={<MovieDetails/>}/>
-    </Routes>
+    
+    <>
+      <Routes>
+          <Route path='/'                  element={<MovieTest/>}/>
+          <Route path='/MovieDetails/:id' element={<MovieDetails/>}/>
+      </Routes>
+      <Footer/>
+    </>
   )
 }
 
